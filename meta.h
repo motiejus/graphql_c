c031967bf7b3b7e498b4f8d69e5f51cf2afe554f
Motiejus Jakštys <desired.mta@gmail.com>
2017-05-25 21:02:40 +0300

    Move from pointers to pre-allocated arrays

--- meta.h
+++ meta.h
@@ -1,11 +1,17 @@
 extern int yylineno;
 void yyerror(char *s, ...);
 
+#define N 64
+
 typedef struct {
-    scalars **t_scalar;
-    types **t_type;
-    enums **t_enum;
-    directives **t_directive;
+    scalars t_scalar[N];
+    size_t num_scalars;
+    types t_type[N];
+    size_t num_types;
+    enums t_enum[N];
+    size_t num_enums;
+    directives t_directive[N];
+    size_t num_directives;
 } t_document;
 
 typedef struct {
@@ -16,26 +22,34 @@ typedef struct {
 typedef struct {
 	name *char;
 	description *char;
-	locations *t_directive_location;
-	args **t_input_value;
+	locations t_directive_location[N];
+    size_t num_locations;
+	args t_input_value[N];
+    size_t num_args;
 } t_directive;
 
 typedef struct {
     kind t_typekind;
     name *char;
     description *char;
-    fields **t_field; // object,interface
-    //interfaces **interface; // not supported
-    //possibleTypes **t_type; // for unions
-    enumValues **t_enum_value; // enum
-    inputFields **t_input_value; // object
-    ofType *t_type; // list,nonnull
+    fields t_field[N]; // object,interface
+    size_t num_fields;
+    //interfaces interface[N]; // not supported
+    //size_t num_interfaces;
+    //possibleTypes t_type[N]; // for unions
+    //size_t num_possibleTypes;
+    enumValues t_enum_value[N]; // enum
+    size_t num_enumValues;
+    inputFields t_input_value[N]; // object
+    size_t num_inputFields;
+    ofType t_type; // list,nonnull
 } t_type;
 
 typedef struct {
     name *char;
     description *char;
-    fields **t_enum_field;
+    fields t_enum_field[N];
+    size_t num_fields;
 } t_enum;
 
 typedef struct {
@@ -53,7 +67,7 @@ typedef struct {
 typedef struct {
 	name *char;
 	description *char;
-    type *t_type;
+    type t_type;
 	defaultValue *char;
 } t_input_value;
 
